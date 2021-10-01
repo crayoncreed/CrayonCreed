c4b18c1c35563f59d9640ed8889a0cbe28336987
Uttam UK <uttamuk003@gmail.com>
2021-10-02 00:42:58 +0530

    css changes

--- js/script.js
+++ js/script.js
@@ -11,176 +11,176 @@ $(document).ready(function () {
 	});
 });
 
-$(document).ready(function () {
-	$('#course').owlCarousel({
-		loop: false,
-		// margin: 7,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// $(document).ready(function () {
+// 	$('#course').owlCarousel({
+// 		loop: false,
+// 		// margin: 7,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 5
-			}
-		}
-	});
-	$('#explore').owlCarousel({
-		loop: false,
-		margin: 10,
-		nav: false,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 2
+// 			},
+// 			1000: {
+// 				items: 5
+// 			}
+// 		}
+// 	});
+// 	$('#explore').owlCarousel({
+// 		loop: false,
+// 		margin: 10,
+// 		nav: false,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 4
-			}
-		}
-	});
-	$('#series').owlCarousel({
-		loop: false,
-		margin: 10,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 2
+// 			},
+// 			1000: {
+// 				items: 4
+// 			}
+// 		}
+// 	});
+// 	$('#series').owlCarousel({
+// 		loop: false,
+// 		margin: 10,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 4
-			}
-		}
-	});
-	$('#creators').owlCarousel({
-		loop: false,
-		// margin: 10,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 2
+// 			},
+// 			1000: {
+// 				items: 4
+// 			}
+// 		}
+// 	});
+// 	$('#creators').owlCarousel({
+// 		loop: false,
+// 		// margin: 10,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 5
-			}
-		}
-	});
-	$('#clients').owlCarousel({
-		loop: false,
-		// margin: 10,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 2
+// 			},
+// 			1000: {
+// 				items: 5
+// 			}
+// 		}
+// 	});
+// 	$('#clients').owlCarousel({
+// 		loop: false,
+// 		// margin: 10,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 2
-			}
-		}
-	});
-	$('#media').owlCarousel({
-		loop: false,
-		margin: 10,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 2
+// 			},
+// 			1000: {
+// 				items: 2
+// 			}
+// 		}
+// 	});
+// 	$('#media').owlCarousel({
+// 		loop: false,
+// 		margin: 10,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 3
-			},
-			600: {
-				items: 4
-			},
-			1000: {
-				items: 7
-			}
-		}
-	});
-	$('#university').owlCarousel({
-		loop: false,
-		margin: 10,
-		nav: true,
-		// rewind: true,
-		dots: true,
-		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-		autoplay: false,
-		// autoplaySpeed: 100,
-		// autoPlay: 20000,
-		// center:true,
-		responsive: {
+// 			0: {
+// 				items: 3
+// 			},
+// 			600: {
+// 				items: 4
+// 			},
+// 			1000: {
+// 				items: 7
+// 			}
+// 		}
+// 	});
+// 	$('#university').owlCarousel({
+// 		loop: false,
+// 		margin: 10,
+// 		nav: true,
+// 		// rewind: true,
+// 		dots: true,
+// 		navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+// 		autoplay: false,
+// 		// autoplaySpeed: 100,
+// 		// autoPlay: 20000,
+// 		// center:true,
+// 		responsive: {
 
-			0: {
-				items: 1
-			},
-			600: {
-				items: 1
-			},
-			1000: {
-				items: 1
-			}
-		}
-	});
-});
+// 			0: {
+// 				items: 1
+// 			},
+// 			600: {
+// 				items: 1
+// 			},
+// 			1000: {
+// 				items: 1
+// 			}
+// 		}
+// 	});
+// });
 
 
 
@@ -229,55 +229,55 @@ $(document).ready(function () {
 
 
 
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-sr.reveal('.home-title', {
-	duration: 2000,
-	origin: 'bottom',
-	distance: '110px',
-	opacity: 0.3,
-	delay: 0,
-	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-	// rotate: { x: 0, y: 10, z: 0 },
-});
+// sr.reveal('.home-title', {
+// 	duration: 2000,
+// 	origin: 'bottom',
+// 	distance: '110px',
+// 	opacity: 0.3,
+// 	delay: 0,
+// 	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+// 	// rotate: { x: 0, y: 10, z: 0 },
+// });
 
-sr.reveal('.home-subtitle', {
-	duration: 2000,
-	origin: 'bottom',
-	distance: '100px',
-	delay: 100,
-	opacity: 0,
-});
+// sr.reveal('.home-subtitle', {
+// 	duration: 2000,
+// 	origin: 'bottom',
+// 	distance: '100px',
+// 	delay: 100,
+// 	opacity: 0,
+// });
 
-sr.reveal('.home-btn', {
-	duration: 2000,
-	origin: 'bottom',
-	distance: '100px',
-	delay: 200,
-	opacity: 0,
-});
-
-
-sr.reveal('.choose-title', {
-	duration: 1500,
-	origin: 'bottom',
-	distance: '70px',
-	viewFactor: 0.01,
-	delay: 0,
-	opacity: 0.1,
-	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-});
+// sr.reveal('.home-btn', {
+// 	duration: 2000,
+// 	origin: 'bottom',
+// 	distance: '100px',
+// 	delay: 200,
+// 	opacity: 0,
+// });
 
 
-sr.reveal('.choose-content', {
-	duration: 1900,
-	origin: 'bottom',
-	distance: '40px',
-	viewFactor: 0.01,
-	delay: 100,
-	opacity: 0.6,
-	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
-});
+// sr.reveal('.choose-title', {
+// 	duration: 1500,
+// 	origin: 'bottom',
+// 	distance: '70px',
+// 	viewFactor: 0.01,
+// 	delay: 0,
+// 	opacity: 0.1,
+// 	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+// });
+
+
+// sr.reveal('.choose-content', {
+// 	duration: 1900,
+// 	origin: 'bottom',
+// 	distance: '40px',
+// 	viewFactor: 0.01,
+// 	delay: 100,
+// 	opacity: 0.6,
+// 	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+// });
 
 
 // sr.reveal('.service-subtitle', {
